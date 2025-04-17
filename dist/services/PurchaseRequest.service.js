03fa10f0cb13504193a13f23cec213d24bf273a6
kevinelia1303 <kevin.elia.santoso@gmail.com>
2025-04-17 16:40:53 +0700

    feature: fix get all purchase request 9

--- dist/services/PurchaseRequest.service.js
+++ dist/services/PurchaseRequest.service.js
@@ -50,7 +50,7 @@ function GetAllPR(_a) {
             //   .offset((page - 1) * pageSize)
             const PRRepo = data_source_1.KevDB.getRepository(PurchaseRequest_entity_1.PurchaseRequest);
             const result = yield PRRepo.createQueryBuilder("PurchaseRequest")
-                .leftJoinAndSelect("item.ItemidId", "item")
+                // .leftJoinAndSelect("item.ItemidId", "item")
                 .getMany();
             return result;
         }
